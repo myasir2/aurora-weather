bf207bbab630eedf5521f98c5bd44224b92bd4a0
myasir2 <myasir_merchant@hotmail.com>
2024-10-17 23:55:17 -0400

    Configured link between the model and Lambda packages to use the generated model files, and added linter to the Lambda package

---
+++ WeatherDataProviderLambda/src/index.ts
@@ -0,0 +1,14 @@
+import { GetWeatherDataRequest } from "@myasir/aurora-weather-data-provider"
+
+class Test {
+
+    constructor() {
+        const request: GetWeatherDataRequest = {
+            longitude: 0.1,
+            latitude: 0.1,
+        }
+
+        const text = "test"
+    }
+
+}
