b818a74cff2279ac91b848f153bcf09d40f13640
myasir2 <myasir_merchant@hotmail.com>
2024-10-19 23:45:38 -0400

    Updated smithy dependencies to work with API Gateway integration

--- WeatherDataProviderLambdaModel/build.gradle.kts
+++ WeatherDataProviderLambdaModel/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.0.20"
-    id("software.amazon.smithy") version "0.6.0" // Apply the Smithy plugin
+    java
+    id("software.amazon.smithy.gradle.smithy-base") version "1.1.0"
 }
 
 group = "ca.myasir"
@@ -11,20 +11,29 @@ repositories {
 }
 
 dependencies {
-    // Smithy model generation tools and libraries
-    implementation("software.amazon.smithy:smithy-model:1.30.0")
-    implementation("software.amazon.smithy:smithy-utils:1.30.0")
-    implementation("software.amazon.smithy:smithy-aws-traits:1.30.0")
-    implementation("software.amazon.smithy:smithy-validation-model:1.30.0")
+    // Smithy dependencies required during model build process
+    smithyBuild("software.amazon.smithy:smithy-aws-apigateway-traits:1.52.0")
+    smithyBuild("software.amazon.smithy:smithy-aws-traits:1.52.0")
+    smithyBuild("software.amazon.smithy:smithy-model:1.52.0")
+    smithyBuild("software.amazon.smithy:smithy-utils:1.52.0")
+    smithyBuild("software.amazon.smithy:smithy-validation-model:1.52.0")
+    smithyBuild("software.amazon.smithy:smithy-aws-endpoints:1.52.0")
+    smithyBuild("software.amazon.smithy:smithy-openapi-traits:1.52.0")
 
-    // TypeScript codegen plugin
-    implementation("software.amazon.smithy.typescript:smithy-aws-typescript-codegen:0.24.0")
 
-    // OpenAPI plugin for API Gateway support
-    implementation("software.amazon.smithy:smithy-openapi:1.30.0")
+    // TypeScript and OpenAPI plugins
+    smithyBuild("software.amazon.smithy.typescript:smithy-aws-typescript-codegen:0.24.0")
+    smithyBuild("software.amazon.smithy:smithy-aws-apigateway-openapi:1.52.0")
 
-    // Kotlin dependencies
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    // Smithy dependencies required for IDE to work with smithy files
+    implementation("software.amazon.smithy:smithy-aws-apigateway-traits:1.52.0")
+    implementation("software.amazon.smithy:smithy-model:1.52.0")
+    implementation("software.amazon.smithy:smithy-aws-traits:1.52.0")
+    implementation("software.amazon.smithy:smithy-utils:1.52.0")
+    implementation("software.amazon.smithy:smithy-aws-traits:1.52.0")
+    implementation("software.amazon.smithy:smithy-validation-model:1.52.0")
+    implementation("software.amazon.smithy:smithy-aws-endpoints:1.52.0")
+    implementation("software.amazon.smithy:smithy-openapi-traits:1.52.0")
 }
 
 java {
@@ -33,20 +42,6 @@ java {
     }
 }
 
-kotlin {
-    jvmToolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
-}
-
-// Disable JAR generation as we don't require it
-tasks.named("smithyBuildJar") {
-    enabled = false
-}
-
-// Configure Smithy build tasks
-tasks.register("smithyBuild", Exec::class) {
-    group = "smithy"
-    description = "Run Smithy build"
-    commandLine("smithy", "build")  // This runs the Smithy build command
+tasks.build {
+    dependsOn("smithyBuild")
 }
--- WeatherDataProviderLambdaModel/src/main/smithy/main.smithy
+++ WeatherDataProviderLambdaModel/src/main/smithy/main.smithy
@@ -1,32 +1,71 @@
-$version: "1.0"
+$version: "2.0"
 
 namespace ca.myasir.auroraweatherservice.weatherdataprovider
 
+use aws.api#service
+use aws.apigateway#integration
+use aws.apigateway#requestValidator
 use aws.auth#sigv4
 use aws.protocols#restJson1
 use smithy.framework#ValidationException
 
-@sigv4(name: "execute-api")
+@service(sdkId: "API Gateway", arnNamespace: "weatherdataproviderservice")
 @restJson1
+@sigv4(name: "weatherdataproviderservice")
+@requestValidator("full")
 service WeatherDataProviderService {
-    version: "2018-05-10",
+    version: "2018-05-10"
     operations: [
-        GetWeatherData
-    ],
+        GetWeatherApiData
+        GetXWeatherApiData
+    ]
 }
 
-@http(code: 200, method: "POST", uri: "/weather")
-operation GetWeatherData {
-    input: GetWeatherDataRequest,
-    output: GetWeatherDataResponse,
-    errors: [ValidationException]
+@http(code: 200, method: "POST", uri: "/weather-api")
+@integration(type: "aws_proxy", httpMethod: "POST", uri: "WeatherApiHandler", passThroughBehavior: "when_no_match")
+operation GetWeatherApiData {
+    input: GetWeatherDataRequest
+    output: GetWeatherDataResponse
+    errors: [
+        ValidationException
+    ]
+}
+
+@http(code: 200, method: "POST", uri: "/x-weather")
+@integration(type: "aws_proxy", httpMethod: "POST", uri: "XWeatherHandler", passThroughBehavior: "when_no_match")
+operation GetXWeatherApiData {
+    input: GetWeatherDataRequest
+    output: GetWeatherDataResponse
+    errors: [
+        ValidationException
+    ]
 }
 
 structure GetWeatherDataRequest {
-    longitude: Double,
+    @required
+    longitude: Double
+    @required
     latitude: Double
 }
 
 structure GetWeatherDataResponse {
-    temperature: Double
+    forecast: WeatherDataList
+}
+
+list WeatherDataList {
+    member: WeatherData
+}
+
+structure WeatherData {
+    date: String
+    temp: Double
+    minTemp: Double
+    maxTemp: Double
+    windSpeed: Double
+    windDirectionDegree: Double
+    humidity: Integer
+    dewpoint: Integer
+    uv: Integer
+    visibility: Integer
+    weatherIconUrl: String
 }
